03f2f7b4fcba96c9d4102495fe0b2fd89adddc56
Holly Rossmann <rossmah@clarkson.edu>
2023-03-09 14:17:32 -0500

    first commit

---
+++ src/routes/gallery/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let data;
+  const {artworks} = data;
+</script>
+
+<article>
+  {#each artworks as { title, artist_title, image_id, date_display, medium_display }}
+  <figure>
+    <img src="https://www.artic.edu/iiif/2/{image_id}/full/843,/0/default.jpg" alt="">
+    <figcaption><strong>{title}</strong> by {artist_title}<br>{date_display} | {medium_display}</figcaption>
+  </figure>
+  {/each}
+</article>
+
+
+<style>
+  article {
+    padding: 2em;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 2em;
+  }
+</style>
